1732292b76afc27bd41dfc079a30432173843740
Aayush-kumar-ctrl <proudindian519@gmail.com>
2025-11-15 20:08:20 +0530

    products.js

--- sneakers-store/frontend/products.js
+++ sneakers-store/frontend/products.js
@@ -62,12 +62,6 @@ const products = [
     image: "./assets/sunglasses.jpg",
     description: "Stylish Lenin Shirt For Daily Use."
   },
-  {
-    id: 5,
-    name: "Lenin Shirt",
-    price: 2499,
-    image: "./assets/sunglasses.jpg",
-    description: "Stylish Lenin Shirt For Daily Use."
-  },
+ 
 ];
 
