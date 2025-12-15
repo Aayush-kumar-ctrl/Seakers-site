a43963848c0e150b80c7c6656001cbb850c264fe
Aayush-kumar-ctrl <proudindian519@gmail.com>
2025-12-15 22:42:34 +0530

    js

--- sneakers-store/frontend/products.js
+++ sneakers-store/frontend/products.js
@@ -41,14 +41,7 @@ const products = [
     image: "./assets/leatherjacket.jpg",
     description: " Premium leather jacket for a sophisticated retro look."
   },
-   {
-    id: 6,
-    name: "Denim Jeans",
-    price: 3299,
-    image: "./assets/denimjeans.jpg",
-    description: " Premium leather jacket for a sophisticated retro look."
-  },
-   
+  
   
 ];
 
