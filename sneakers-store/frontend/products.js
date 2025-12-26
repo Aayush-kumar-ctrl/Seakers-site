56fcf91a6f73101fcdfdb233d84552849e20fc81
Aayush-kumar-ctrl <proudindian519@gmail.com>
2025-12-26 15:22:34 +0530

    js

--- sneakers-store/frontend/products.js
+++ sneakers-store/frontend/products.js
@@ -41,6 +41,13 @@ const products = [
     image: "./assets/tshirts.jpg",
     description: "Comfortable retro t-shirt with classic prints."
   },
+  {
+    id: 6,
+    name: "chelsea Boots",
+    price: 899,
+    image: "./assets/rxinjkt.jpg",
+    description: "Durable rexin jacket with a vintage design for all-weather style."
+  },
   
 ];
 
