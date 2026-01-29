836d1cecbcf8b5f17f34ab2b6183e0032075cff5
Aayush-kumar-ctrl <proudindian519@gmail.com>
2026-01-29 13:10:29 +0530

    js

--- sneakers-store/frontend/products.js
+++ sneakers-store/frontend/products.js
@@ -74,7 +74,6 @@ const products = [
     name: "Formal Shoes",
     price:4499,
     image: "./assets/formalshoes.jpg",
-    description: "Elegant formal shoes with a classic retro touch for special occasions."
   },
 
 
